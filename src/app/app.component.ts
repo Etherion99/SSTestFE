0ed0c632e26ffd980fba1d74b393b200e4f27a0c
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2022-04-11 09:37:39 -0500

    other validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,74 +48,118 @@ export class AppComponent {
   }
 
   public addHandler({ sender }:any, formInstance: any) {
-    formInstance.reset();
-    this.closeEditor(sender);
+    if(this.editedRowIndex == -1){
+      formInstance.reset();
+      this.closeEditor(sender);
 
-    let row:any = {};
+      let row:any = {};
 
-    this.columns.forEach((col:any) => {
-      switch(col.dataType){
-        case "Int":
-          row[col.header] = 0;
-          break;
-        case "String":
-        case "Date":          
-          row[col.header] = '';
-          break;
-      }
-      
-    });
+      this.columns.forEach((col:any) => {
+        switch(col.dataType){
+          case "Int":
+            row[col.header] = 0;
+            break;
+          case "String":
+          case "Date":          
+            row[col.header] = '';
+            break;
+        }
+        
+      });
 
-    sender.addRow(row);
+      sender.addRow(row);
+    }else{
+      alert('Termina de editar este registro antes de agregaruno nuevo');
+    }    
   }
 
   public saveHandler({ sender, rowIndex, dataItem, isNew }:any) {
-    if(isNew){
-      this.api.createTableRow(this.selectedTable._id, { content: dataItem }).subscribe(data => {
-        sender.closeRow(rowIndex);
-  
-        this.editedRowIndex = -1;
+    let valid = true;
 
-        this.selectTable();
-      });
-    }else{
+    this.columns.forEach(col => {
+      if(!this.validateField(col.header, dataItem[col.header], col.dataType, col.format, col.required)){
+        alert('Hay un error en el campo ' + col.header);
+        valid = false;
+      }
+    });
+
+    if(valid){
       const id = dataItem.id;
       delete dataItem['id'];
 
-      this.api.updateTableRow(id, { content: dataItem }).subscribe(data => {
-        sender.closeRow(rowIndex);
-        
-        this.selectTable();
-      });
+      if(isNew){
+        this.api.createTableRow(this.selectedTable._id, { content: dataItem }).subscribe(data => {
+          sender.closeRow(rowIndex);
+          this.editedRowIndex = -1;
+  
+          this.selectTable();
+        });
+      }else{
+        this.api.updateTableRow(id, { content: dataItem }).subscribe(data => {
+          sender.closeRow(rowIndex);
+          this.editedRowIndex = -1;
+          
+          this.selectTable();
+        });
+      }
     }
   }
 
   public editHandler({ sender, rowIndex, dataItem }: any) {
-    this.closeEditor(sender);
+    if(this.editedRowIndex == -1){
+      this.closeEditor(sender);
 
-    sender.editRow(rowIndex);
+      sender.editRow(rowIndex);
+      this.editedRowIndex = rowIndex;
+    }else{
+      alert('Termina de editar este registro antes de editar uno diferente');
+    }    
+  }
+
+  public cancelHandler({ sender, rowIndex }:any) {
+    this.closeEditor(sender, rowIndex);
+    this.editedRowIndex = -1;
   }
 
   public removeHandler({ dataItem }:any) {
-    console.log('id', dataItem.id);
-    this.api.removeTableRow(this.selectedTable._id, dataItem.id).subscribe(data => {
-      this.selectTable();
-    });
+    if (confirm("¿Estás seguro que deseas eliminar este registro?") == true) {
+      this.api.removeTableRow(this.selectedTable._id, dataItem.id).subscribe(data => {
+        this.selectTable();
+      });
+    }    
   }
-
-  /*public cancelHandler({ sender, rowIndex }) {
-    this.closeEditor(sender, rowIndex);
-  }
-
-  
-
-  public removeHandler({ dataItem }) {
-    this.editService.remove(dataItem);
-  }*/
 
   private closeEditor(grid:any, rowIndex = this.editedRowIndex) {
     grid.closeRow(rowIndex);
 
     this.editedRowIndex = -1;
+  }
+
+  private validateField(field:string, value:any, dataType: string, format:string = "", required:boolean): boolean{
+    if(required){
+      switch(dataType){
+        case "Int":
+          return !isNaN(value) && value !== "";
+        case "String":
+          return value !== "";
+        case "Date":
+          let regEx = /^\d{4}-\d{2}-\d{2}$/;
+          return value.match(regEx);
+        default:
+          return true;
+      }
+    }else{
+      return true;
+    }
+  }
+
+  public getType(dataType: string): string{
+    const dict:any = {
+      "Int": "number",
+      "String": "text",
+      "Date": "date"
+    }
+
+    return dict[dataType];
   }
 }
